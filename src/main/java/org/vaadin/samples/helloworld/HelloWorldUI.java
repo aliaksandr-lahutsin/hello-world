a79608ec2d4458f2a5e504359758a953fb3e1f70
aliaksandr-lahutsin <35953755+aliaksandr-lahutsin@users.noreply.github.com>
2018-03-23 15:03:59 +0200

    Update HelloWorldUI.java

--- src/main/java/org/vaadin/samples/helloworld/HelloWorldUI.java
+++ src/main/java/org/vaadin/samples/helloworld/HelloWorldUI.java
@@ -25,7 +25,7 @@ public class HelloWorldUI extends UI {
     public static class Servlet extends VaadinServlet {
     }
 
-    /*
+    
     @Override
     protected void init(VaadinRequest request) {
         final VerticalLayout layout = new VerticalLayout();
@@ -55,6 +55,5 @@ public class HelloWorldUI extends UI {
         layout.addComponent(button);
         layout.addComponent(clickCounterLabel = new Label("Clicks: 0"));
     }
-    */
-
+    
 }
